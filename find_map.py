ff4d3b7347238d90d267d37aca1660326307a60a
NgoHaiLOng <ngohailong1991@gmail.com>
2023-03-25 11:24:15 +0700

    'fixed_code'
    Signed-off-by: NgoHaiLOng <ngohailong1991@gmail.com>

--- find_map.py
+++ find_map.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
 
 __doc__ = """
-Yêu cầu:
-Viết script tìm 50 quán bia / quán nhậu / quán bar /
+Script tìm 50 quán bia / quán nhậu / quán bar /
 nhà hàng quanh toạ độ của lớp học (lên google map để lấy) với bán kính 2KM.
 Ghi kết quả theo định dạng JSON vào file pymi_beer.geojson
 
 Sử dụng Google Map API
 https://developers.google.com/places/web-service/
 
-Chú ý: phải tạo "token" để có thể truy cập API -
-phải tạo tài khoản google cloud,
-cần có thẻ thanh toán online quốc tế (VISA/Mastercard).
-Học viên không có thẻ thì đi làm thẻ.
-
-Chú ý: giữa mỗi trang kết quả phải đợi để lấy tiếp.
-
-Chú ý: tránh đặt ngược lat/long
-
 - Kết quả trả về lưu theo format JSON,
 với mỗi điểm là một GeoJSON point (https://leafletjs.com/examples/geojson/),
-up file này lên GitHub để xem bản đồ kết quả.
 """
 
 import requests
